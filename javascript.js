c2dba82ee6793a844f8d385f6f9400f5d9e0e39f
Avery Kyle Arenas <kyruken@gmail.com>
2022-06-22 20:00:25 -0700

    create gameFlow module

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 //Goal is to have little to no global variables or functions
 
+//gameBoard contains the literal gameboard and functions for the gameboard
 const gameBoard = (() => {
     const gameBoardArray = [];
 
@@ -7,7 +8,8 @@ const gameBoard = (() => {
 
     const addO = () => { gameBoardArray.unshift('O')};
 
-    const showGameBoard = () => {
+    //add checkWin function that checks winning patterns
+    const displayGameBoard = () => {
         for (let x = 0; x < gameBoardArray.length; x++) {
             console.log(`Index ${x} with value ${gameBoardArray[x]}`);
         }
@@ -15,13 +17,45 @@ const gameBoard = (() => {
     return {
         addX: addX,
         addO: addO,
-        showGameBoard: showGameBoard
+        display: displayGameBoard
     }
 
 })();
 
-const Player = () => {
-    const score = 0;
+//Module for controlling player turn and the game
+const gameFlow = (() => {
+    //turnCounter = 1 means player 1's turn, turnCounter = 2 means player 2's turn 
+    let turnCounter = 1;
 
+    const displayPlayerTurn = () => {console.log(`It is Player ${turnCounter}'s turn`)}
+
+    const playerMove = () => {
+        if (turnCounter === 1) {
+            gameBoard.addX();
+            changeTurn();
+        }
+        else {
+            gameBoard.addO();
+            changeTurn();
+        }
+    }
+    const changeTurn = () => {
+        if (turnCounter === 1) {
+            turnCounter = 2;
+        }
+        else {
+            turnCounter = 1;
+        }
+    }
+
+    return {
+        display: displayPlayerTurn,
+        move: playerMove
+    }
+
+})();
+
+const Player = (tag) => {
+    const name = `Player ${tag}`;
 
 };
